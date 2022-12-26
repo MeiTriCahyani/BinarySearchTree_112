43b5de9ca13e54a4bb766b9f8febc2ab93aa466e
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2022-12-26 18:04:01 +0700

    public  node

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -8,9 +8,18 @@ namespace BinarySearchTree
 {
     class Node
     {
-        public string info;
-        public Node lchild;
-        public Node rchild;
-        //constructor for the node class
+       public string info;
+       public Node lchild;
+       public Node rchild;
+       //constructor for the node class
+       public Node(string i, Node l, Node r)
+       {
+            info = i;
+            lchild = l;
+            rchild = r;
+
+       }
+
     }
+    
 }
